41138449682845e277144d61e4a89f9639681466
Silviu <xsylver@yahoo.com>
2017-02-14 00:32:22 +0200

    Initial commit

---
+++ src/main/java/controller/UploadController.java
@@ -0,0 +1,10 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by vlasceanusilviu on 2/14/17.
+ */
+@RestController
+public class UploadController {
+}
